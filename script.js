1b84f984e5aefb61ea9fe408f78f88ac0f58e0c6
Marcefloyd <marce88fenix@gmail.com>
2026-04-11 02:34:00 -0300

    Actualización de “Vibrando Acuario”:
    Reorganicé la estructura de la página, arreglé las ruedas (ya no se cruzan), mejoré el responsive, ajusté tamaños con variables CSS y limpié la estética de los signos. También dejé lista la sección de contenido y el footer, y avancé con el menú hamburguesa.

--- script.js
+++ script.js
@@ -108,3 +108,9 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("activo");
+});
